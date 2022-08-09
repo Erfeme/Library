0b603a839ece44c991bcd2f119df176352406b8a
enod <win_1118@hotmail.com>
2022-08-09 10:04:45 -0500

    Added some functions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # git_test
 First GitHub repo
 Hello
+
+This is the first time i'm doing this, is kinda weird in scared hahhaha D'x
---
+++ js/app.js
@@ -0,0 +1,19 @@
+function Library(){
+
+}
+
+Library.prototype.sayInfo = function(){
+    return this.info
+}
+
+function Book(title, author, pages, read, info){
+
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = info;
+
+}
+
+Book.prototype = Object.create(Library.prototype);
