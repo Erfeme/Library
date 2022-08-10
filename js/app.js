6f0d22d2c177585bc6cdd98f41a84c5dd43362ff
Erfeme <win_1118@hotmail.com>
2022-08-09 19:36:41 -0500

    In the way to create a function to spawn the books into the page, already added some books to test

--- js/app.js
+++ js/app.js
@@ -1,4 +1,67 @@
-let myLibrary = [];
+const btn = document.querySelector('.newBook');
+
+btn.addEventListener('click',(e)=>{
+    
+})
+
+let myLibrary = [
+    {
+        title: 'Las mil y una noches',
+        author: 'Al Jamar Zajir',
+        pages: 150,
+        read: true,
+        info: 'Una de las obras del islam más importantes de todos los tiempos, probablemente la más influyente en occidente'
+    },
+    {
+        title: 'Las mil y una noches',
+        author: 'Al Jamar Zajir',
+        pages: 150,
+        read: true,
+        info: 'Una de las obras del islam más importantes de todos los tiempos, probablemente la más influyente en occidente'
+    },
+    {
+        title: 'Las mil y una noches',
+        author: 'Al Jamar Zajir',
+        pages: 150,
+        read: true,
+        info: 'Una de las obras del islam más importantes de todos los tiempos, probablemente la más influyente en occidente'
+    },
+    {
+        title: 'Las mil y una noches',
+        author: 'Al Jamar Zajir',
+        pages: 150,
+        read: true,
+        info: 'Una de las obras del islam más importantes de todos los tiempos, probablemente la más influyente en occidente'
+    },
+    {
+        title: 'Las mil y una noches',
+        author: 'Al Jamar Zajir',
+        pages: 150,
+        read: true,
+        info: 'Una de las obras del islam más importantes de todos los tiempos, probablemente la más influyente en occidente'
+    },
+    {
+        title: 'Las mil y una noches',
+        author: 'Al Jamar Zajir',
+        pages: 150,
+        read: true,
+        info: 'Una de las obras del islam más importantes de todos los tiempos, probablemente la más influyente en occidente'
+    },
+    {
+        title: 'Las mil y una noches',
+        author: 'Al Jamar Zajir',
+        pages: 150,
+        read: true,
+        info: 'Una de las obras del islam más importantes de todos los tiempos, probablemente la más influyente en occidente'
+    }
+];
+
+spawnLibrary = ()=>{
+    var div = document.createElement('div');
+    var title = document.createElement('h2');
+    
+    console.log(div);
+}
 
 function Library(){
 
@@ -18,4 +81,6 @@ function Book(title, author, pages, read, info){
 
 }
 
-Book.prototype = Object.create(Library.prototype);
+
+
+Book.prototype = Object.create(Library.prototype); 
